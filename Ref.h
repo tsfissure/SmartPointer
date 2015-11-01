1c5d210fd982244e1a13ebe0517011de0eb8c32d
tsfissure <tsfissure@qq.com>
2015-11-01 21:39:04 +0800

    Create Ref.h
    所有需要智能指针的基类

---
+++ Ref.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "FissureDefinition.h"
+
+//////////////////////////////////////////////////////////////////////////
+//所有需要智能指针的基类
+class Ref {
+
+public:
+	Ref();
+	virtual			~Ref();
+
+public:
+	void			retain();
+	void			release();
+	unsigned int	getRefCount() const;
+
+private:
+	unsigned int	refCount;
+};
